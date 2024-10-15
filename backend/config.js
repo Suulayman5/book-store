719a86e74b9c8fdd5d61db6c9998332e87ecbd70
ajah suleiman <ajahsuleiman@gmail.com>
2024-10-15 17:29:46 +0100

    setup database

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 4000;
+
+export const mongoDB= 'mongodb+srv://ajahsuleiman:Kg6XkQj8KjTiMINu@cluster0.8dnrd.mongodb.net/book-store?retryWrites=true&w=majority&appName=Cluster0'
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import cors from 'cors'
-import { PORT } from './config.js'
+import { PORT, mongoDB } from './config.js'
+import mongoose from 'mongoose'
 
 
 const app = express()
 
-
-app.listen(PORT, ()=>{
-    console.log('port connected');
+mongoose.connect(mongoDB)
+ .then(()=>{
+    console.log('connected to database');
+    app.listen(PORT, ()=>{
+        console.log(`port connected on ${PORT}`);
+    })
+ })
+ .catch((error)=>{
+    console.log(error);
     
-})
+ })
