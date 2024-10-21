301cb810b2b8c561a7bdaaa103a0c21dfd1f8431
ajah suleiman <ajahsuleiman@gmail.com>
2024-10-21 21:46:39 +0100

    create route for getting books from database

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express, { request, response } from 'express'
 import cors from 'cors'
 import { PORT, mongoDB } from './config.js'
 import mongoose from 'mongoose'
+import { Book } from './models/bookModel.js'
 
 
 const app = express()
@@ -19,12 +20,28 @@ app.post('/books', async (req,res)=>{
             author: req.body.author,
             publishYear: req.body.publishYear
         }
-        const book = await book.create(newBook)
+        const book = await Book.create(newBook)
 
         return res.status(201).send(book)
     } catch (error) {
         console.log(error.message);
         res.status(500).send({message: error.message})
+        
+    }
+})
+
+app.get('/books', async (req,res)=>{
+    try {
+        const books = await Book.find({}) 
+        res.status(200).json({
+            count: books.length,
+            data: books
+        })
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({
+            message: error.message
+        })
         
     }
 })
