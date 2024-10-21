095ffe112e86c3a32f71c6f09c112b44bd1c69da
ajah suleiman <ajahsuleiman@gmail.com>
2024-10-21 21:35:16 +0100

    create route for creating a new book

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,33 @@
-import express from 'express'
+import express, { request, response } from 'express'
 import cors from 'cors'
 import { PORT, mongoDB } from './config.js'
 import mongoose from 'mongoose'
 
 
 const app = express()
+app.use(express.json())
+
+app.post('/books', async (req,res)=>{
+    try {
+        if (!req.body.title || !req.body.author || !req.body.publishYear) {
+            return res.status(400).send({
+                message:  'All feilds requires'
+            })
+        }    
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear
+        }
+        const book = await book.create(newBook)
+
+        return res.status(201).send(book)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message: error.message})
+        
+    }
+})
 
 mongoose.connect(mongoDB)
  .then(()=>{
